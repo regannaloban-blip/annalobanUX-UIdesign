ed2303784f7289eeae0a6b12ceaf05430b110f3f
Anna <ann.loban@gmail.com>
2026-07-14 21:26:59 +0200

    v0.3.10 add dev-only agentation

---
+++ src/components/DevAgentation.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Agentation } from "agentation";
+
+export function DevAgentation() {
+  return <Agentation endpoint="http://localhost:4747" />;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <App />,
-);
+createRoot(document.getElementById("root")).render(<App />);
+
+if (import.meta.env.DEV) {
+  import("./components/DevAgentation.jsx").then(({ DevAgentation }) => {
+    const container = document.createElement("div");
+    container.id = "agentation-dev-root";
+    document.body.appendChild(container);
+    createRoot(container).render(<DevAgentation />);
+  });
+}
